262527b58c4131c36deaa59096a1538348474bf0
imagio <andrew@taskheroics.com>
2023-03-12 17:08:49 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Result, ok, Ok, err, Err, fromThrowable } from './result'
-export { ResultAsync, okAsync, errAsync, fromPromise, fromSafePromise } from './result-async'
-export { combine, combineWithAllErrors, combineAsync, combineWithAllErrorsAsync } from './utils'
+export { Result, ok, Ok, err, Err, fromThrowable } from './result.ts'
+export { ResultAsync, okAsync, errAsync, fromPromise, fromSafePromise } from './result-async.ts'
+export { combine, combineWithAllErrors, combineAsync, combineWithAllErrorsAsync } from './utils.ts'
