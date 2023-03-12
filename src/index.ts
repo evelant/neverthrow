cc653130364d3d77d339e41c5d34acc976367049
imagio <imagio@mailfence.com>
2023-03-12 17:04:47 -0400

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Result, ok, Ok, err, Err, fromThrowable } from './result'
 export { ResultAsync, okAsync, errAsync, fromPromise, fromSafePromise } from './result-async'
-export { combine, combineWithAllErrors } from './utils'
+export { combine, combineWithAllErrors, combineAsync, combineWithAllErrorsAsync } from './utils'
